26657d8efedb16c76f3963625b0b20771b4df540
홍준성 <powerstar13@hanmail.net>
2022-04-16 02:07:15 +0900

    git init > WebFlux 시작

---
+++ src/test/java/study/practice/webflux/WebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package study.practice.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
